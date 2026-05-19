f62b04fe967db7fbb80a9a3102e1cf10f1503e50
Deelaksha NJ <deelaksha22@gmail.com>
2026-05-19 21:53:41 +0530

    Learning C Language

---
+++ LogicalOperators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    
+    printf("Enter two numbers:\n");
+    scanf("%d %d",&a,&b);
+
+    if(a>0 && b>0){
+        printf("Both numbers are positive\n");
+    }
+    else if(a>0 || b>0){
+        printf("At least one number is positive\n");
+    }
+    else{
+        printf("Neither number is positive\n");
+    }
+    return 0;
+}   
